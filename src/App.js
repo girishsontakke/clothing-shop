5517c78352ff58f702566c0cc9a6afe2647cc5b6
girishsontakke <girishsontakke7@gmail.com>
2020-08-27 18:41:34 +0530

    homepage of website

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+import HomePage from "./homepage.component";
 
 class App extends Component {
   render() {
-    return <div>Hello World</div>;
+    return (
+      <>
+        <HomePage />
+      </>
+    );
   }
 }
 
