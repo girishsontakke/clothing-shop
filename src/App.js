be522fd5b64f746ec29fdf15256e7fa275885825
girishsontakke <girishsontakke7@gmail.com>
2020-08-30 21:59:44 +0530

    add route for shop page

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import React from "react";
 import { Route } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
-
-const HatsPage = () => {
-  return (
-    <div>
-      <h1> {`hats page`.toUpperCase()} </h1>
-    </div>
-  );
-};
+import ShopPage from "./pages/homepage/shop/shop.component";
 
 const App = () => {
   return (
     <>
       <Route exact path="/" component={HomePage} />
-      <Route path="/shop/hats" component={HatsPage} />
+      <Route path="/shop" component={ShopPage} />
     </>
   );
 };
