0e83d95cf5f07b9e0f6a9696f5eea188ac6eb664
girishsontakke <girishsontakke7@gmail.com>
2020-08-31 14:30:52 +0530

    make the routes unique with switch

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
@@ -7,8 +7,10 @@ import ShopPage from "./pages/shop/shop.component";
 const App = () => {
   return (
     <>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/shop" component={ShopPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </>
   );
 };
