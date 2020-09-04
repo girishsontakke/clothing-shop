1e915536cd5466e551b1b7ae0182690a4b6ac994
girishsontakke <girishsontakke7@gmail.com>
2020-09-04 19:08:40 +0530

    make sign in button dependent on user login

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,8 +2,9 @@ import React from "react";
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { Link } from "react-router-dom";
 import "./header.styles.scss";
+import { auth } from "../../firebase/firebase.utils";
 
-const Header = () => (
+const Header = ({ currentUser }) => (
   <div className="header">
     <Link className="logo-container" to="/">
       <Logo className="logo" />
@@ -15,9 +16,15 @@ const Header = () => (
       <Link className="option" to="/contact">
         {"contact".toUpperCase()}
       </Link>
-      <Link className="option" to="/signin">
-        {"Sign in".toUpperCase()}
-      </Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          {"sign out".toUpperCase()}
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          {"Sign in".toUpperCase()}
+        </Link>
+      )}
     </div>
   </div>
 );
