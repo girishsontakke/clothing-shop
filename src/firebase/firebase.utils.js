01d86c6f24a02c534ee28f5949a74cc53997f865
girishsontakke <girishsontakke7@gmail.com>
2020-09-06 15:28:40 +0530

    store authenticated users to firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,25 @@ const config = {
   measurementId: "G-4XCFWCKJS7",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`user/${userAuth.uid}`);
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      userRef.set({ displayName, email, createdAt, ...additionalData });
+    } catch (error) {
+      console.log("error occured during creating user", error.message);
+    }
+  }
+  return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
