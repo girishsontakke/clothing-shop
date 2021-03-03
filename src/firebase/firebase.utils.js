0bbf7e18eaf2778d81b19e646ae4f9c310776fb4
girishsontakke <girishsontakke7@gmail.com>
2021-03-03 14:12:42 +0530

    hoist firebase initialization

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -14,6 +14,8 @@ const config = {
   measurementId: "G-4XCFWCKJS7",
 };
 
+firebase.initializeApp(config);
+
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -31,8 +33,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   }
   return userRef;
 };
-
-firebase.initializeApp(config);
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
