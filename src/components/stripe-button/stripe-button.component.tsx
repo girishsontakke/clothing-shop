9b4f12feb86eef160b4564ab06d96f5aad095e3b
girishsontakke <girishsontakke7@gmail.com>
2021-06-24 10:26:39 +0530

    add stripe integration

---
+++ src/components/stripe-button/stripe-button.component.tsx
@@ -0,0 +1,30 @@
+import StripeCheckout, { Token } from "react-stripe-checkout";
+
+interface IProps {
+  price: number;
+}
+const StripeCheckoutButton: React.FC<IProps> = ({ price }) => {
+  const onToken = (token: Token) => {
+    console.log(token);
+    alert("Payment Successful");
+  };
+  const publishableKey =
+    "pk_test_51J5Yq0SCR2r1huHpjb7GzjD4knq20XKQa0kbSTVXbqsgm89jJbyL8GxfyrH39MACzTqRBEosTzNsE2i4d1nFoEul00lkVXwR1u";
+  const priceForStripe = 100 * price;
+  return (
+    <StripeCheckout
+      token={onToken}
+      stripeKey={publishableKey}
+      label="Pay Now"
+      name="CRWN Clothing"
+      description={`Your total is $${price}`}
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+    />
+  );
+};
+
+export default StripeCheckoutButton;
--- src/pages/checkout/checkout.page.tsx
+++ src/pages/checkout/checkout.page.tsx
@@ -3,8 +3,9 @@ import "./checkout.styles.scss";
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
 import { selectCartItems, selectCartTotal } from "../../redux/cart/cartSlice";
-
 import CheckoutItem from "../../components/checkout-item/checkout-item.component";
+import StripeCheckoutButton from "../../components/stripe-button/stripe-button.component";
+
 import { CartItemType } from "../../types/models";
 import { ReduxState } from "../../types/reduxState";
 
@@ -37,6 +38,11 @@ const CheckoutPage: React.FC<Iprops> = ({ cartItems, total }) => {
         <CheckoutItem key={cartItem.id} item={cartItem} />
       ))}
       <div className="total"> Total: ${total}</div>
+      <div className="test-warning">
+        *Please use following test credit card for payment* <br />
+        4242 4242 4242 4242 - Exp: 01/2022 - CVV: 123
+      </div>
+      <StripeCheckoutButton price={total} />
     </section>
   );
 };
