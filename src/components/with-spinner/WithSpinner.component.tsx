4cbdf90f088fd25c3a04ce1082785f918b69fc0e
girishsontakke <girishsontakke7@gmail.com>
2021-06-27 10:24:06 +0530

    assign type to HOC withSpinner

--- src/components/with-spinner/WithSpinner.component.tsx
+++ src/components/with-spinner/WithSpinner.component.tsx
@@ -7,7 +7,7 @@ interface Iprops {
 
 // Higher Order Component
 const WithSpinner = (WrappedComponent: React.FC<any>): React.FC<Iprops> => {
-  const Spinner: React.FC<any> = ({ isLoading, ...otherProps }) => {
+  const Spinner: React.FC<Iprops> = ({ isLoading, ...otherProps }) => {
     return isLoading ? (
       <div className="spinner-overlay">
         <div className="spinner-container"></div>
